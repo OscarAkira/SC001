1478fc6810eb22049cbe66342d4029837f68438a
Oscar <93373046+OscarAkira@users.noreply.github.com>
2022-01-10 23:36:35 +0800

    Update weather_master.py

--- weather_master.py
+++ weather_master.py
@@ -11,33 +11,34 @@ run in the Assignment 2 Handout.
 
 # This constant controls when to stop
 EXIT = -100
+STANDARD = 16
 
 def main():
 	"""
 	TODO:
 	"""
+	total = 0
 	print('StandCode \"Weather Master 4.0”!')
 	data = int(input('Next Temperature: (or '+str(EXIT)+' to quit)? '))
 	if data == EXIT:
 		print('No temperatures were entered.')
 	else:
 		maximum = data
-		min = data
-		standard = 16
-		total = 0
+		mini = data
 		while True:
 			data = int(input('Next Temperature: (or '+str(EXIT)+' to quit)? '))
 			if data == EXIT:
 				break
 			if data > maximum:
 				maximum = data
-			if data < min:
-				min = data
-			if data < standard:
+			if data < mini:
+				mini = data
+			if data < STANDARD:
+				STANDARD = data
 				total += 1
 		print("Highest temperature = "+str(maximum))
-		print("Lowest temperature = "+str(min))
-		average = (maximum + min) / 2
+		print("Lowest temperature = "+str(mini))
+		average = (maximum + mini) / 2
 		print("Average = "+str(average))
 		print(str(total)+' cold day(s)')
 
